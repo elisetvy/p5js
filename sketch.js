e973439cc223a3b197fd3d48dd3196f0c99794dc
Elise Ngo <elisetvy@gmail.com>
2024-09-15 18:06:18 -0700

    Draw grass

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function draw() {
   background(135, 206, 235);
 
   // circle in the center with a width of 100
-  // circle(200, 200, 100)
+  // circle(200, 200, 100);
 
   // when mouse is pressed, circles turn black
   // if (mouseIsPressed) {
@@ -16,11 +16,19 @@ function draw() {
   // }
 
   // white circles drawn at mouse position
-  // circle(mouseX, mouseY, 100)
+  // circle(mouseX, mouseY, 100);
 
   // sun in the top right corner w/ yellow fill + thick orange outline
   fill("yellow");
   stroke("orange");
   strokeWeight(20);
-  circle(550, 50, 100)
+  circle(550, 50, 100);
+
+  // reset outline
+  stroke(0); // black outline
+  strokeWeight(1);
+
+  // grass
+  fill("green");
+  rect(0, 200, 600, 200); // x, y, width, height
 }
