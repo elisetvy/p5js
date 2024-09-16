6cd5d12f5397af9d231240c05a9b1b681579c8cf
Elise Ngo <elisetvy@gmail.com>
2024-09-15 18:02:07 -0700

    Draw sun

--- sketch.js
+++ sketch.js
@@ -1,20 +1,26 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400); // width, height
 }
 
 function draw() {
-  background(220);
+  background(135, 206, 235);
 
   // circle in the center with a width of 100
   // circle(200, 200, 100)
 
   // when mouse is pressed, circles turn black
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
 
   // white circles drawn at mouse position
-  circle(mouseX, mouseY, 100)
+  // circle(mouseX, mouseY, 100)
+
+  // sun in the top right corner w/ yellow fill + thick orange outline
+  fill("yellow");
+  stroke("orange");
+  strokeWeight(20);
+  circle(550, 50, 100)
 }
