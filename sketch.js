8728a57c1365260a6acecc1c2d4c8d3cc6c22ff2
Elise Ngo <elisetvy@gmail.com>
2024-09-15 17:41:06 -0700

    Draw cicrcles at mouse position + fill w/ different color when mouse is pressed

--- sketch.js
+++ sketch.js
@@ -6,5 +6,15 @@ function draw() {
   background(220);
 
   // circle in the center with a width of 100
-  circle(200, 200, 100)
+  // circle(200, 200, 100)
+
+  // when mouse is pressed, circles turn black
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  // white circles drawn at mouse position
+  circle(mouseX, mouseY, 100)
 }
