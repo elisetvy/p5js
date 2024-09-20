45b6af9b34daf2975486b2894138b80de8313f50
Elise Ngo <elisetvy@gmail.com>
2024-09-20 14:10:56 -0700

    Make crescent moon

--- sketch.js
+++ sketch.js
@@ -10,6 +10,11 @@ function draw() {
   stroke(0);
   circle(350, 50, 100); // x, y, diameter of circle
 
+  // overlapping navy circle to make crescent moon
+  stroke("navy");
+  fill("navy");
+  circle(320, 50, 100);
+
 // displays the x and y position of the mouse on the canvas
 fill(255) // white text
 text(`${mouseX}, ${mouseY}`, 20, 20);
