852250d3c57dc120090b75ea6640381f218056c4
Elise Ngo <elisetvy@gmail.com>
2024-09-20 14:13:53 -0700

    Draw mountains

--- sketch.js
+++ sketch.js
@@ -15,6 +15,12 @@ function draw() {
   fill("navy");
   circle(320, 50, 100);
 
+  // big gray mountains
+  stroke(0);
+  fill(80);
+  triangle(-40, 300, 75, 100, 250, 300); //  point 1 x, point 1 y, point 2 x, point 2 y, etc
+  triangle (100, 300, 300, 100, 500, 300);
+
 // displays the x and y position of the mouse on the canvas
 fill(255) // white text
 text(`${mouseX}, ${mouseY}`, 20, 20);
